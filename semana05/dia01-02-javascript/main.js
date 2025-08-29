fc7ac8fe3d2b0a3b9269790b9eb70220e25aa5cc
Victor Villazón <victorzn23@gmail.com>
2025-08-28 22:28:26 -0500

    semana05, dia01 02, javascript

--- semana05/dia01-02-javascript/main.js
+++ semana05/dia01-02-javascript/main.js
@@ -1,1 +1,71 @@
-console.log('Hola a todos')
+// COMENTARIOS
+
+// console.log('Hola a todos')
+
+/*
+  Esto
+  es
+  un
+  comentario
+  de
+  múltiples
+  líneas
+*/
+
+// TIPOS DE DATOS
+
+// PRIMITIVOS: Number, String, Boolean (true, false), undefined, null, BigInt, Symbol...
+
+// NUMBERS
+
+console.log(20)
+console.log(20.50)
+console.log(-36)
+
+console.log(typeof 20)
+console.log(typeof 20.50)
+console.log(typeof -36)
+
+// STRING o Cadenas de texto
+
+console.log('Gabriel')
+console.log("Bryan")
+console.log('500')
+
+console.log(typeof 'Gabriel')
+console.log(typeof "Bryan")
+console.log(typeof '500')
+
+// BOOLEAN -> verdadero(true) o falso(false)
+
+console.log(true)
+console.log(false)
+
+console.log(typeof true)
+
+// UNDEFINED, lo asigna javascript
+
+let nombre // No hemos asignado un valor a la variable
+
+console.log(nombre)
+console.log(typeof nombre)
+
+// NULL
+
+// let apellido = undefined // ❌
+let apellido = null
+
+console.log(apellido)
+console.log(typeof apellido)
+
+// BigInt (Tipo de dato primitivo)
+// Symbol (Tipo de dato primitivo)
+
+// Object (Tipo de datos no primitivos)
+// Function (Tipo de datos no primitivos)
+// Array (Tipo de datos no primitivos)
+// Set (Tipo de datos no primitivos)
+// etc.
+
+
+// VARIABLES Y CONSTANTES
