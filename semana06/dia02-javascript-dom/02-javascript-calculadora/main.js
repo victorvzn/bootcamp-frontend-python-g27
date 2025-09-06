3161b850bd45bfaecf71951780864efb9cc3820e
Victor Villazón <victorzn23@gmail.com>
2025-09-05 21:51:01 -0500

    semana06, dia02 02, javascript - dom + calculadora

--- semana06/dia02-javascript-dom/02-javascript-calculadora/main.js
+++ semana06/dia02-javascript-dom/02-javascript-calculadora/main.js
@@ -1,1 +1,29 @@
-console.log('Hola JS')
+let numeroActual = '0'
+let operador = ''
+let operando = ''
+
+// Consultando un elemento a la vez
+const inputDisplay = document.querySelector('#inputDisplay')
+// Consultando multiples elementos a la vez
+const buttons = document.querySelectorAll('.button')
+
+console.log(buttons)
+
+// EVENTOS
+buttons.forEach(function(button) {
+
+  // button.addEventListener(NOMBRE_EVENTO, FUNCION)
+  button.addEventListener('click', function(event) {
+    // console.log('Hice click!', event.target)
+
+    const buttonSelected = event.target
+    const buttonText = buttonSelected.textContent
+
+    console.log(buttonText)
+
+    // Se presionó algún número
+    numeroActual = Number(numeroActual + buttonText)
+
+    inputDisplay.value = numeroActual
+  })
+})
