811002bbcf93c91dbc035cbd742ffd5f6794634c
Victor Villazón <victorzn23@gmail.com>
2025-09-05 22:50:33 -0500

    semana06, dia02 02, javascript - dom + calculadora

--- semana06/dia02-javascript-dom/03-javascript-todo-app/main.js
+++ semana06/dia02-javascript-dom/03-javascript-todo-app/main.js
@@ -1,1 +1,18 @@
-console.log('Hola JS')
+const taskInput = document.getElementById('taskInput')
+const taskAdd = document.querySelector('.task__add')
+const taskList = document.querySelector('.task__list')
+
+// EVENTOS
+
+// ELEMENTO.addEventListener(NOMBRE_EVENTO, CALLBACK)
+
+taskAdd.addEventListener('click', function(event) {
+  // Se ejecutará cuandohagamos click en el botón 'Añadir tarea'
+  console.log('Click!!!', taskInput.value)
+
+  // MANEJO DEL DOM: Forma de crear elementos dinámicamente con el DOM de Javascript
+  const button = document.createElement('button')
+  button.textContent = 'Hola soy un botón'
+  console.log({button})
+  document.body.appendChild(button)
+})
