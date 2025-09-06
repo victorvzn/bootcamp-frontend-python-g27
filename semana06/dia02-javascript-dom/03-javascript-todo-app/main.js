54e578bcf178402ac0cf60b07dac5823fdb0c3a2
Victor Villazón <victorzn23@gmail.com>
2025-09-05 23:01:19 -0500

    semana06, dia02 02, javascript - dom + calculadora

--- semana06/dia02-javascript-dom/03-javascript-todo-app/main.js
+++ semana06/dia02-javascript-dom/03-javascript-todo-app/main.js
@@ -11,8 +11,30 @@ taskAdd.addEventListener('click', function(event) {
   console.log('Click!!!', taskInput.value)
 
   // MANEJO DEL DOM: Forma de crear elementos dinámicamente con el DOM de Javascript
+  // const button = document.createElement('button')
+  // button.textContent = 'Hola soy un botón'
+  // console.log({button})
+  // document.body.appendChild(button)
+
+  // DONE: 01 Añadir el elemento li al elmento con la clase task__list
+  const li = document.createElement('li')
+
+  // TODO: 01 Añadir un checkbox al li
+
+  // DONE: 02 Añadir el elemento span al elemento li
+  const span = document.createElement('span')
+  span.textContent = taskInput.value
+  li.appendChild(span)
+
+  // DONE: 03 Añadir el elemento button al elemento li
   const button = document.createElement('button')
-  button.textContent = 'Hola soy un botón'
-  console.log({button})
-  document.body.appendChild(button)
+  button.textContent = 'Borrar'
+  li.appendChild(button)
+
+  taskList.appendChild(li)
+
+  // TODO: 02 Limpiar la caja de texto después de crear la tarea
+
 })
+
+// TODO: 03 permitir al botón borrar remover una tarea de la lista
