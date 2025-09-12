9b74b4378ad1a4b0bbc61ef6fcf56823a9bb8beb
Victor Villazón <victorzn23@gmail.com>
2025-09-11 23:02:30 -0500

    semana07, dia01 01, javascript - todo mejorado

--- semana07/dia01-01-todo-app-tailwind-mejorado/main.js
+++ semana07/dia01-01-todo-app-tailwind-mejorado/main.js
@@ -1,1 +1,56 @@
-console.log('Hola JS')
+const taskInput = document.querySelector('.task__input')
+const taskButtonClear = document.querySelector('.task__clear')
+const taskList = document.querySelector('.task__list')
+
+const tasks = [
+  {
+    id: 'tarea-1',
+    title: 'Estudiar Javascript',
+    completed: true
+  },
+  {
+    id: 'tarea-2',
+    title: 'Salir al receso',
+    completed: true
+  },
+  {
+    id: 'tarea-3',
+    title: 'Resolver el reto de la semana',
+    completed: false
+  }
+]
+
+function renderTasks(tasks = []) {
+  // console.log('Renderizando tasks...', tasks)
+
+  let list = ''
+
+  tasks.forEach(task => {
+    console.log(task)
+    list = list + `
+      <li class="flex justify-center items-center gap-4 py-1">
+        <input type="checkbox" />
+        <div class="w-full">
+          ${task.title}
+        </div>
+        <div class="flex gap-2">
+          <button
+            class="border border-green-700 font-medium text-sm px-2 py-1 text-green-700 rounded-lg hover:bg-green-700 hover:text-white duration-300"
+          >
+            ✏
+          </button>
+          <button
+            class="border border-red-700 font-medium text-sm px-2 py-1 text-red-700 rounded-lg hover:bg-red-700 hover:text-white duration-300"
+          >
+            ❌
+          </button>
+        </div>
+      </li>
+    `
+  })
+  
+
+  taskList.innerHTML = list
+}
+
+renderTasks(tasks)
