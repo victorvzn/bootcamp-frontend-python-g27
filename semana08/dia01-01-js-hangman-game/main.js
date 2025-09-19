0e979e5d08718403335f2c9a451cea843ae5ac1d
Victor Villazón <victorzn23@gmail.com>
2025-09-18 20:26:03 -0500

    semana08, dia01 01, javascript - hangman game

--- semana08/dia01-01-js-hangman-game/main.js
+++ semana08/dia01-01-js-hangman-game/main.js
@@ -1,1 +1,24 @@
-console.log('Hola JS')
+const palabraOculta = document.querySelector('.ahorcado__palabra-oculta')
+const botones = document.querySelector('.ahorcado__botones')
+const resultado = document.querySelector('.ahorcado__resultados')
+const botonReiniciar = document.querySelector('.ahorcado__reiniciar')
+
+// palabraOculta.textContent = 'HOLA A TODOS'
+
+const PALABRA_RESPUESTA = 'JAVASCRIPT'
+
+const ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('') // Un arreglo
+
+const renderizarAlfabeto = () => {
+  let contenido = ''
+
+  ALFABETO.forEach(letra => {
+    contenido = contenido + `
+      <button class="bg-blue-500 p-2 text-white font-bold text-xl cursor-pointer hover:bg-blue-600 duration-300 rounded">${letra}</button>
+    `
+  })
+
+  botones.innerHTML = contenido
+}
+
+renderizarAlfabeto()
