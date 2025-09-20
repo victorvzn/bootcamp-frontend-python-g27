90c50763ab8fb0c2faaaeb767a2cf277ffd25f37
Victor Villazón <victorzn23@gmail.com>
2025-09-19 22:24:39 -0500

    semana08, dia02 01, javascript - fetch api + promises

--- semana08/dia02-02-js-fetch-ejercicio/main.js
+++ semana08/dia02-02-js-fetch-ejercicio/main.js
@@ -31,3 +31,29 @@ fetch(url)
 
   
 // TODO: Renderizar los nombres de los planetas de la siguiente ruta (API) y mostrar su nombre, descripción y su imagen
+
+const renderPlanetas = (planetas) => {
+  const planetasDiv = document.querySelector('#planetas')
+
+  let html = ''
+
+  planetas.forEach(character => {
+    html += `
+      <article>
+        <h2>${character.id} - ${character.name}</h2>
+        <p>${character.description}</p>
+        <img src="${character.image}" height="200" />
+      </article>
+    `
+  });
+
+  planetasDiv.innerHTML = html
+}
+
+fetch('https://dragonball-api.com/api/planets')
+  .then(response => response.json())
+  .then(data => {
+    // console.log(data)
+    
+    renderPlanetas(data.items)
+  })
