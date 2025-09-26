5a876674eed56d43abe469bb8c26efc2fa48fe70
Victor Villazón <victorzn23@gmail.com>
2025-09-26 08:14:55 -0500

    semana09, dia01 01, javascript movie app crusd + json server + nodejs

--- semana09/dia01-01-js-movie-app-crud-json-server-nodejs/public/services.js
+++ semana09/dia01-01-js-movie-app-crud-json-server-nodejs/public/services.js
@@ -1,7 +1,21 @@
+const url = 'http://localhost:3000/movies'
+
 export const fetchMovies = async () => {
-  const url = 'http://localhost:3000/movies'
-  
-  const response = await fetch(url)
+  const response = await fetch(url) // Esperar la respuesta del servidor (asincrona)
+
+  return await response.json() // Esperar a la conversión de JSON a Objeto JS (asincrona)
+}
+
+export const createMovie = async (form) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(form) // El body siempre tiene que ser una cadena de texto
+  }
+
+  const response = await fetch(url, options)
 
   return await response.json()
 }
