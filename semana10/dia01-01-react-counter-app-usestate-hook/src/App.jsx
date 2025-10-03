140fcbd3f6fbb4df2f9364c004e075bea9da7099
Victor Villazón <victorzn23@gmail.com>
2025-10-02 21:55:31 -0500

    semana10, dia01 01, react counter usestate hook

--- semana10/dia01-01-react-counter-app-usestate-hook/src/App.jsx
+++ semana10/dia01-01-react-counter-app-usestate-hook/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  // const contador = 8
+  // [valor, funcion] = useState(inicializador)
+
+  const [contador, setContador] = useState(0) //
+
+  const increment = () => {
+    setContador(contador + 1)
+  }
+
+  const decrement = () => {
+    setContador(contador - 1)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Contador</h1>
+
+      <button onClick={increment}>+1</button>
+      <span>{contador}</span>
+      <button onClick={decrement}>-1</button>
+    </div>
   )
 }
+
+// const App = () => {
+//   let contador = 3
+
+//   console.log(contador)
+
+//   return (
+//     <div>
+//       <button onClick={() => {
+//         contador = contador + 1
+//         console.log(contador)
+//       }}>{contador}</button>
+//     </div>
+//   )
+// }
 
 export default App
