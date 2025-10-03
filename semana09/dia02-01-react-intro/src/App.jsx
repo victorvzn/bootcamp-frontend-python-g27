fdde673d42875a7e2ff6bb5a536a9e24ada627b7
Victor Villazón <victorzn23@gmail.com>
2025-10-02 20:03:03 -0500

    semana09, dia02 01, react intro

--- semana09/dia02-01-react-intro/src/App.jsx
+++ semana09/dia02-01-react-intro/src/App.jsx
@@ -1,12 +1,37 @@
-// 01 - COMPONENTE BASE USANDO FUNCTION
+// 03 - COMPONENTE USANDO MÚLTIPLES LÍNEAS + FRAGMENTS (<></>)
 
-function App() {
+const App = () => {
+  // Cuerpo de la función donde va la lógica del componente
+
   return (
-    <h1>Vite + React + JSX - hola</h1>
+    <>
+      <h1>Hola React!</h1>
+      <p>Estamos aprendiendo sobre componentes.</p>
+    </>
   )
 }
 
+// 02 - COMPONENTE BASE USANDO ARROW FUNCTIONS
+
+// const App = () => <h1>Vite + React + JSX - Arrow</h1>
+
+// const App = () => {
+//   return (
+//     <h1>Vite + React + JSX - Arrow</h1>
+//   )
+// }
+
 export default App
+
+// 01 - COMPONENTE BASE USANDO FUNCTION
+
+// function App() {
+//   return (
+//     <h1>Vite + React + JSX - hola</h1>
+//   )
+// }
+
+// export default App
 
 
 // 00 - ESM
