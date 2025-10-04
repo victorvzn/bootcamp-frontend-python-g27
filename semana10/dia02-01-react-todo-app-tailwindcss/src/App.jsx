952ca5255bd708f5ed54623dc7251be72ca4b074
Victor Villazón <victorzn23@gmail.com>
2025-10-03 20:02:53 -0500

    semana10, dia02 01, react todo app tailwindcss

--- semana10/dia02-01-react-todo-app-tailwindcss/src/App.jsx
+++ semana10/dia02-01-react-todo-app-tailwindcss/src/App.jsx
@@ -1,9 +1,31 @@
-import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const DEFAULT_TODOS = [
+    {
+      id: '1',
+      title: 'Aprender Javascript',
+      completed: true
+    },
+    {
+      id: '2',
+      title: 'Aprender CSS',
+      completed: false
+    },
+    {
+      id: '3',
+      title: 'Aprender React.js + Tailwind CSS',
+      completed: true
+    }
+  ]
+
+  const [todos, setTodos] = useState(DEFAULT_TODOS)
+
   return (
-      
-    <h1 className='logo' style={{ color: 'red', backgroundColor: '#ade' }}>Hola React.js!</h1>
+    <main>
+      {/* <h1 className='logo' style={{ color: 'red', backgroundColor: '#ade' }}>Hola React.js!</h1> */}
+      <h1>Todo App + React + Tailwind</h1>
+    </main>
   )
 }
 
