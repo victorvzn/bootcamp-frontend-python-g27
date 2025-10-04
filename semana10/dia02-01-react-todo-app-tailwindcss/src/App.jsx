31d2cba2bf5ccfa004131170b2c7de90b8fd285c
Victor Villazón <victorzn23@gmail.com>
2025-10-03 20:47:00 -0500

    semana10, dia01 01, react calculadora tailwindcss

--- semana10/dia02-01-react-todo-app-tailwindcss/src/App.jsx
+++ semana10/dia02-01-react-todo-app-tailwindcss/src/App.jsx
@@ -21,18 +21,30 @@ function App() {
 
   const [todos, setTodos] = useState(DEFAULT_TODOS)
 
+  const handleRemove = (id) => {
+    console.log('Click', id)
+    const updatedTodos = todos.filter(todo => todo.id !== id)
+
+    setTodos(updatedTodos) // Remplazamos el estado todos con los todos actulizados
+  }
+
   return (
     <main>
-      {/* <h1 className='logo' style={{ color: 'red', backgroundColor: '#ade' }}>Hola React.js!</h1> */}
       <h1>Todo App + React + Tailwind</h1>
 
       <ul>
-        {todos.map(todo => {
-          return <li>{todo.title}</li>
+        {todos.map((todo, index) => {
+          return <li key={todo.id}>
+            {todo.title}
+
+            <button onClick={() => handleRemove(todo.id)}>❌</button>
+          </li>
         })}
       </ul>
 
       <pre>{JSON.stringify(todos, null, 2)}</pre>
+
+      {/* <h1 className='logo' style={{ color: 'red', backgroundColor: '#ade' }}>Hola React.js!</h1> */}
     </main>
   )
 }
