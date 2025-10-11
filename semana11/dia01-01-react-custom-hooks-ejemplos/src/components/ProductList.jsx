afbc5800cf33e6584a2fc5be54fe8b42a369fd3c
Victor Villazón <victorzn23@gmail.com>
2025-10-10 20:49:44 -0500

    semana11, dia01 01, react custom hooks ejemplos

--- semana11/dia01-01-react-custom-hooks-ejemplos/src/components/ProductList.jsx
+++ semana11/dia01-01-react-custom-hooks-ejemplos/src/components/ProductList.jsx
@@ -1,24 +1,37 @@
 import { useEffect, useState } from "react"
 
 const fetchProducts = () => {
-  return fetch('https://dummyjson.com/products')
+  return fetch('https://dummyjson.com/products?delay=2000')
     .then(reponse => reponse.json())
 }
 
 const ProductList = () => {
   const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     console.log('Aparece por primera vez')
-
+    setLoading(true)
     // Se ejecuta al cargar por primera vez el componente gracias al arreglo vacío ([])
     fetchProducts()
       .then(data => {
         console.log(data.products)
         setProducts(data.products)
       })
+      .finally(() => {
+        setLoading(false)
+      })
 
   }, [])
+
+  // Conditional rendering
+  if(loading) {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
 
   return (
     <div>
