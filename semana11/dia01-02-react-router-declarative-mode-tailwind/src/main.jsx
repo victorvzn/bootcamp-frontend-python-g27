c0562d0e40f9d61589c4ff4f8174ae0d16b43c5e
Victor Villazón <victorzn23@gmail.com>
2025-10-10 21:56:38 -0500

    semana11, dia01 02, react router declarative mode tailwind

--- semana11/dia01-02-react-router-declarative-mode-tailwind/src/main.jsx
+++ semana11/dia01-02-react-router-declarative-mode-tailwind/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { BrowserRouter, Route, Routes } from 'react-router'
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+
+      <Route path='/login' element={<h1>Login Page</h1>} />
+      <Route path='/register' element={<h1>Register Page</h1>} />
+      {/* <App /> */}
+    </Routes>
+  </BrowserRouter>
 )
