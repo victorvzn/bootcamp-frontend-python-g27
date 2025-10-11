968dcc1d278846d5ffadb76527a08d7e2854f9ef
Victor Villazón <victorzn23@gmail.com>
2025-10-10 20:14:49 -0500

    semana11, dia01 01, react custom hooks ejemplos

--- semana11/dia01-01-react-custom-hooks-ejemplos/src/components/ProductList.jsx
+++ semana11/dia01-01-react-custom-hooks-ejemplos/src/components/ProductList.jsx
@@ -5,3 +5,21 @@ const ProductList = () => {
 }
 
 export default ProductList
+
+
+
+// useEffect. permite ejecutar código cuando algo cambia o cuando el componente aparece en pantalla (montar, mount)
+
+// Lo encontramos al llamar APIs, escuchar eventes, ejecutar lógica cuando cambian los estados(ej. counter)
+
+// Sintaxis
+
+// useEffect(FUNCTION, DEPENDENCIAS)
+
+/*
+useEffect(() => {
+  // Código que se ejecuta segun las dependencias
+}, [])
+*/
+
+// Cuando el arreglo de las depencidas está vacío significa que el componente aparece por primera vez.
