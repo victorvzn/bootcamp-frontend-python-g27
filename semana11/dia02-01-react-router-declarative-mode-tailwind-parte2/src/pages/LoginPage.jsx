dd63f0ee5e59eaebb6cd7838074011f6923460e2
Victor Villazón <victorzn23@gmail.com>
2025-10-16 20:27:33 -0500

    semana11, dia02 01, react router declarative mode tailwind parte 2 (layouts)

--- semana11/dia02-01-react-router-declarative-mode-tailwind-parte2/src/pages/LoginPage.jsx
+++ semana11/dia02-01-react-router-declarative-mode-tailwind-parte2/src/pages/LoginPage.jsx
@@ -1,6 +1,35 @@
+import { useState } from "react"
+
 const LoginPage = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault() // Evita que el formulario actualice la página
+
+    console.log(email, password)
+  }
+
   return (
-    <div>LoginPage - hola</div>
+    <>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email
+          <input type="email" name="email" onChange={(event) => setEmail(event.target.value)} />
+        </label>
+
+        <label>
+          Password
+          <input type="password" name="password" onChange={(event) => setPassword(event.target.value)} />
+        </label>
+
+        <input type="submit" value='Login' />
+
+        <pre>{JSON.stringify({ email, password })}</pre>
+      </form>
+    </>
   )
 }
 
