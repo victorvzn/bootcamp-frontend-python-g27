256f05ca50c1470035f5b659bae1c481ab05e13e
Victor Villazón <victorzn23@gmail.com>
2025-10-16 20:16:44 -0500

    semana11, dia02 01, react router declarative mode tailwind parte 2 (layouts)

--- semana11/dia02-01-react-router-declarative-mode-tailwind-parte2/src/layouts/AdminLayout.jsx
+++ semana11/dia02-01-react-router-declarative-mode-tailwind-parte2/src/layouts/AdminLayout.jsx
@@ -1,11 +1,22 @@
-import { Outlet } from "react-router"
+import { Link, Outlet } from "react-router"
 
 const AdminLayout = () => {
   return (
     <>
-      <h3>AdminLayout</h3>
+      <header className="bg-yellow-300 p-4">
+        <div className="container mx-auto flex justify-between">
+          <h1>React router (ADMIN)</h1>
 
-      <Outlet />
+          <nav className="flex gap-4">
+            <Link to='/'>Home</Link>
+            <Link to='/login'>Logout</Link>
+          </nav>
+        </div>
+      </header>
+
+      <main className="container mx-auto m-6">
+        <Outlet />
+      </main>
     </>
   )
 }
--- semana11/dia02-01-react-router-declarative-mode-tailwind-parte2/src/layouts/AuthLayout.jsx
+++ semana11/dia02-01-react-router-declarative-mode-tailwind-parte2/src/layouts/AuthLayout.jsx
@@ -1,11 +1,23 @@
-import { Outlet } from "react-router"
+import { Link, Outlet } from "react-router"
 
 const AuthLayout = () => {
   return (
     <>
-      <h1>Soy el layout del LOGIN/REGISTER</h1>
+      <header className="bg-emerald-300 p-4">
+        <div className="container mx-auto flex justify-between">
+          <h1>React router - Auth</h1>
 
-      <Outlet />
+          <nav className="flex gap-4">
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
+            <Link to='/'>Home</Link>
+          </nav>
+        </div>
+      </header>
+
+      <main className="container mx-auto m-6">
+        <Outlet />
+      </main>
     </>
   )
 }
