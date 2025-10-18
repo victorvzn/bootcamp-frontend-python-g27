f2d7a9b6f48b76be473aa78f997c259569da110a
Victor Villazón <victorzn23@gmail.com>
2025-10-17 22:38:00 -0500

    semana11, dia02 01, react router declarative mode tailwind parte 2 (layouts)

--- semana12/dia01-01-react-redux-counter-shopping-cart-app/src/App.jsx
+++ semana12/dia01-01-react-redux-counter-shopping-cart-app/src/App.jsx
@@ -1,8 +1,25 @@
+import { useState, useEffect } from "react"
+
 import Counter from "./components/Counter"
+import ProductList from "./components/ProductList"
 
 const App = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      .then(data => setProducts(data.products))
+  }, [])
+
   return (
     <main>
+      <section className="flex gap-2">
+        <ProductList products={products} />
+
+        {/* <ShoppingCart /> */}
+      </section>
+
       <Counter />
     </main>
   )
