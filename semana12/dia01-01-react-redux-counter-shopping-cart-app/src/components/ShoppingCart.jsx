b0943a5bb69b75c21194a09665f5165e8b7eae5b
Victor Villazón <victorzn23@gmail.com>
2025-10-23 19:47:43 -0500

    semana11, dia02 01, react router declarative mode tailwind parte 2 (layouts)

--- semana12/dia01-01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
+++ semana12/dia01-01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
@@ -1,25 +1,31 @@
 const ShoppingCart = () => {
   return (
-    <section>
-      <h3>Carrito de compras</h3>
+    <section className="w-56 p-2">
+      <h3 className="text-2xl mb-2 text-center">Carrito de compras</h3>
 
-      <div>
-        <button>
+      <div className="mb-2">
+        <button
+          className="bg-violet-400 text-white px-3 py-2 rounded-lg cursor-pointer w-full hover:bg-violet-600 duration-300"
+        >
           Limpiar carrito
         </button>
       </div>
 
-      <ul>
-        <li>
+      <ul className="flex flex-col gap-3">
+        <li
+          className="flex flex-col gap-2 font-bold bg-slate-200 p-2 rounded-lg shadow"
+        >
           <span>Producto 1</span>
           <span>S/ 50.50 (Qty: 5)</span>
-          <button>
+          <button
+            className="bg-red-400 text-white py-1 rounded-lg cursor-pointer w-full hover:bg-red-600 duration-300"
+          >
             ❌
           </button>
         </li>
       </ul>
 
-      <div>
+      <div className="mt-4 pt-4 font-bold bg-amber-100 px-4 py-2 flex justify-between rounded-lg">
         <strong>TOTAL:</strong> <span>S/ 250.00</span>
       </div>
     </section>
