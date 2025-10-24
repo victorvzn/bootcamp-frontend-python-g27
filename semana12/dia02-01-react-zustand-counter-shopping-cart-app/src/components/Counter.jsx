c077681926c7143f93d9a0e0135af23c767ebbda
Victor Villazón <victorzn23@gmail.com>
2025-10-23 22:37:53 -0500

    semana11, dia02 01, react router declarative mode tailwind parte 2 (layouts)

--- semana12/dia02-01-react-zustand-counter-shopping-cart-app/src/components/Counter.jsx
+++ semana12/dia02-01-react-zustand-counter-shopping-cart-app/src/components/Counter.jsx
@@ -1,4 +1,8 @@
+import { useCounterStore } from '../store/counter.js'
+
 const Counter = () => {
+  const { count, increment, decrement } = useCounterStore()
+
   return (
     <div className="border rounded p-2 flex flex-col gap-2 items-center">
       <h3 className="font-bold">Counter with Zustand</h3>
@@ -11,14 +15,16 @@ const Counter = () => {
         </button>
         <button
           className="bg-green-400 p-2 min-w-14 rounded font-bold cursor-pointer"
+          onClick={decrement}
         >
           -1
         </button>
         <h1 className="bg-slate-300 p-2 min-w-14 rounded font-bold text-center">
-          0
+          {count}
         </h1>
         <button
           className="bg-green-400 p-2 min-w-14 rounded font-bold cursor-pointer"
+          onClick={increment}
         >
           +1
         </button>
---
+++ semana12/dia02-01-react-zustand-counter-shopping-cart-app/src/store/counter.js
@@ -0,0 +1,22 @@
+import { create } from 'zustand'
+
+export const useCounterStore = create(
+  (set) => ({
+    // Estado inicial
+    count: 99,
+    // Actions
+    increment: () => {
+      console.log('incrementando++')
+      set(state => {
+        return {
+          count: state.count + 1
+        }
+      })
+    },
+    decrement: () => {
+      console.log('decrementando++')
+      set(state => ({ count: state.count - 1 }))
+    },
+    // TODO: Implementar la función incrementByValue y usarla en el componente <Counter />
+  })
+)
