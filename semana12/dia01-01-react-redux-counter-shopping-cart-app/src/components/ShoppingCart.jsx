888524de1c9eb7d47353c3b12603e2ab5e8cdab3
Victor Villazón <victorzn23@gmail.com>
2025-10-23 21:40:00 -0500

    semana11, dia02 01, react router declarative mode tailwind parte 2 (layouts)

--- semana12/dia01-01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
+++ semana12/dia01-01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
@@ -7,6 +7,29 @@ const ShoppingCart = () => {
 
   const dispatch = useDispatch()
 
+  const total = cart.reduce((acc, product) => {
+    const qty = product.qty
+    const price = product.price
+    const subtotal = qty * price
+
+    return acc + subtotal
+  }, 0)
+
+  const isCartEmpty = cart.length === 0
+
+  if (isCartEmpty) {
+    return (
+      <section className="w-56 p-2">
+        <h3 className="text-2xl mb-2">Shopping Cart</h3>
+
+        <div className="mb-2 bg-slate-300 h-32 rounded-lg p-4 flex flex-col items-center">
+          🛒
+          <p className="text-lg text-center text-slate-500">Shopping cart empty</p>
+        </div>
+      </section>
+    )
+  }
+
   return (
     <section className="w-56 p-2">
       <h3 className="text-2xl mb-2 text-center">Carrito de compras</h3>
@@ -41,7 +64,7 @@ const ShoppingCart = () => {
       </ul>
 
       <div className="mt-4 pt-4 font-bold bg-amber-100 px-4 py-2 flex justify-between rounded-lg">
-        <strong>TOTAL:</strong> <span>S/ 250.00</span>
+        <strong>TOTAL:</strong> <span>S/ {total}</span>
       </div>
 
       <pre>{JSON.stringify(cart)}</pre>
