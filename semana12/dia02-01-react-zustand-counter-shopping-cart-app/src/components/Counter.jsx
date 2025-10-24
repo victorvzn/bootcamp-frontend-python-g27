a767fed360c04765f43d7695ec2e2f127acecce7
Victor Villazón <victorzn23@gmail.com>
2025-10-23 22:04:15 -0500

    semana11, dia02 01, react router declarative mode tailwind parte 2 (layouts)

---
+++ semana12/dia02-01-react-zustand-counter-shopping-cart-app/src/components/Counter.jsx
@@ -0,0 +1,35 @@
+const Counter = () => {
+  return (
+    <div className="border rounded p-2 flex flex-col gap-2 items-center">
+      <h3 className="font-bold">Counter with Zustand</h3>
+
+      <div className="flex items-center gap-2">
+        <button
+          className="bg-sky-400 p-2 min-w-14 rounded font-bold cursor-pointer"
+        >
+          -5
+        </button>
+        <button
+          className="bg-green-400 p-2 min-w-14 rounded font-bold cursor-pointer"
+        >
+          -1
+        </button>
+        <h1 className="bg-slate-300 p-2 min-w-14 rounded font-bold text-center">
+          0
+        </h1>
+        <button
+          className="bg-green-400 p-2 min-w-14 rounded font-bold cursor-pointer"
+        >
+          +1
+        </button>
+        <button
+          className="bg-sky-400 p-2 min-w-14 rounded font-bold cursor-pointer"
+        >
+          +5
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Counter
