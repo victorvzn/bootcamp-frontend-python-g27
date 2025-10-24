c40ef3c413d4aea8b4f85ea32effb98eaf3adc72
Victor Villazón <victorzn23@gmail.com>
2025-10-23 22:59:46 -0500

    semana11, dia02 01, react router declarative mode tailwind parte 2 (layouts)

--- semana12/dia02-01-react-zustand-counter-shopping-cart-app/src/App.jsx
+++ semana12/dia02-01-react-zustand-counter-shopping-cart-app/src/App.jsx
@@ -1,25 +1,25 @@
 import { useState, useEffect } from "react"
 
 import Counter from "./components/Counter"
-// import ProductList from "./components/ProductList"
-// import ShoppingCart from "./components/ShoppingCart"
+import ProductList from "./components/ProductList"
+import ShoppingCart from "./components/ShoppingCart"
 
 const App = () => {
-  // const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([])
 
-  // useEffect(() => {
-  //   fetch('https://dummyjson.com/products')
-  //     .then(response => response.json())
-  //     .then(data => setProducts(data.products))
-  // }, [])
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      .then(data => setProducts(data.products))
+  }, [])
 
   return (
     <main>
-      {/* <section className="flex gap-2">
+      <section className="flex gap-2">
         <ProductList products={products} />
 
         <ShoppingCart />
-      </section> */}
+      </section>
 
       <Counter />
     </main>
---
+++ semana12/dia02-01-react-zustand-counter-shopping-cart-app/src/components/ShoppingCart.jsx
@@ -0,0 +1,67 @@
+const ShoppingCart = () => {
+  const cart = []
+
+  const total = cart.reduce((acc, product) => {
+    const qty = product.qty
+    const price = product.price
+    const subtotal = qty * price
+
+    return acc + subtotal
+  }, 0)
+
+  const isCartEmpty = cart.length === 0
+
+  if (isCartEmpty) {
+    return (
+      <section className="w-56 p-2">
+        <h3 className="text-2xl mb-2">Shopping Cart</h3>
+
+        <div className="mb-2 bg-slate-300 h-32 rounded-lg p-4 flex flex-col items-center">
+          🛒
+          <p className="text-lg text-center text-slate-500">Shopping cart empty</p>
+        </div>
+      </section>
+    )
+  }
+
+  return (
+    <section className="w-56 p-2">
+      <h3 className="text-2xl mb-2 text-center">Carrito de compras</h3>
+
+      <div className="mb-2">
+        <button
+          className="bg-violet-400 text-white px-3 py-2 rounded-lg cursor-pointer w-full hover:bg-violet-600 duration-300"
+        >
+          Limpiar carrito
+        </button>
+      </div>
+
+      <ul className="flex flex-col gap-3">
+        {cart.map(product => {
+          return (
+            <li
+              className="flex flex-col gap-2 font-bold bg-slate-200 p-2 rounded-lg shadow"
+              key={product.id}
+            >
+              <span>{product.title}</span>
+              <span>S/ {product.price} (Qty: {product.qty})</span>
+              <button
+                className="bg-red-400 text-white py-1 rounded-lg cursor-pointer w-full hover:bg-red-600 duration-300"
+              >
+                ❌
+              </button>
+            </li>
+          )
+        })}
+      </ul>
+
+      <div className="mt-4 pt-4 font-bold bg-amber-100 px-4 py-2 flex justify-between rounded-lg">
+        <strong>TOTAL:</strong> <span>S/ {total}</span>
+      </div>
+
+      <pre>{JSON.stringify(cart)}</pre>
+    </section>
+  )
+}
+
+export default ShoppingCart
