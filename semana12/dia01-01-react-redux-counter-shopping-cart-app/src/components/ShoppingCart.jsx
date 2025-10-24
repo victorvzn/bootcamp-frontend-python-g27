9ff1ada0269c1150d6f7212ee9546aade2c5ef79
Victor Villazón <victorzn23@gmail.com>
2025-10-23 19:36:38 -0500

    semana11, dia02 01, react router declarative mode tailwind parte 2 (layouts)

--- semana12/dia01-01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
+++ semana12/dia01-01-react-redux-counter-shopping-cart-app/src/components/ShoppingCart.jsx
@@ -1,6 +1,28 @@
 const ShoppingCart = () => {
   return (
-    <div>ShoppingCart</div>
+    <section>
+      <h3>Carrito de compras</h3>
+
+      <div>
+        <button>
+          Limpiar carrito
+        </button>
+      </div>
+
+      <ul>
+        <li>
+          <span>Producto 1</span>
+          <span>S/ 50.50 (Qty: 5)</span>
+          <button>
+            ❌
+          </button>
+        </li>
+      </ul>
+
+      <div>
+        <strong>TOTAL:</strong> <span>S/ 250.00</span>
+      </div>
+    </section>
   )
 }
 
